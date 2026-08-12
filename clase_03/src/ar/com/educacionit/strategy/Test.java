1aabe1a6ff612d616ce774253f64073ec32021e1
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-12 12:02:43 -0300

    Empezando con patrón strategy

---
+++ clase_03/src/ar/com/educacionit/strategy/Test.java
@@ -0,0 +1,19 @@
+package ar.com.educacionit.strategy;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Clase 03 - Patrón Strategy");
+		
+		// Patrón Strategy (Patrón Comportamiento)
+		// https://refactoring.guru/es/design-patterns/strategy
+		// Nos permite tener varias formas de hacer lo mismo y poder cambiarlas
+		// sin tocar el código que las usa.
+		// Tenés que pagar algo (PAGAR)
+		// Tarjeta de crédito (Estrategias Tarjeta crédito)
+		// Efectivo (Estrategias Efectivo)
+		// Transferencia (Estrategias Transferencia)
+
+	}
+
+}
