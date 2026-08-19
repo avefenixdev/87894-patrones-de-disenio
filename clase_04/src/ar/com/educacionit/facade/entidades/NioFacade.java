37d156d9031c3ebdce0886b11258479b43b7c821
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-19 10:19:21 -0300

    Terminado con uml incluido el patrón fachada

--- clase_04/src/ar/com/educacionit/facade/entidades/NioFacade.java
+++ clase_04/src/ar/com/educacionit/facade/entidades/NioFacade.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class NioFacade {
 	
@@ -32,5 +34,30 @@ public class NioFacade {
 		System.out.println("Líneas escritas en: " + rutaArchivo.getFileName());
 	}
 	
+	// Leer líneas de una cadena (stream)
+	public void leerLineasContenidas(String palabraClave) throws IOException {
+		try(Stream<String> lineas = Files.lines(rutaArchivo)) {
+			lineas.filter(linea -> linea.contains(palabraClave))
+				.forEach(System.out::println);
+		}
+	}
+	
+	// Copiar archivo a destino
+	public void copiarA(String destinoArchivo) throws IOException {
+		
+		Path destinoRuta = Paths.get(destinoArchivo);
+		
+		if (Files.notExists(destinoRuta.getParent())) {
+			Files.createDirectories(destinoRuta.getParent());
+		}
+		Files.copy(rutaArchivo, destinoRuta, StandardCopyOption.REPLACE_EXISTING);
+		System.out.println("Archivo copiado a: " + destinoRuta.toAbsolutePath());
+	}
+	
+	// Eliminar archivo
+	public void borrar() throws IOException {
+		Files.deleteIfExists(rutaArchivo);
+		System.out.println("Archivo eliminado: " + rutaArchivo.toAbsolutePath());
+	}
 
 }
