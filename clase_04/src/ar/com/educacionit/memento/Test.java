909a2c648413b18a40359ae2d221d386d796868f
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-19 11:15:01 -0300

    Empezando con el patrón de diseño Memento

---
+++ clase_04/src/ar/com/educacionit/memento/Test.java
@@ -0,0 +1,20 @@
+package ar.com.educacionit.memento;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Patrón Memento");
+		
+		// https://refactoring.guru/es/design-patterns/memento
+		// Patrón Memento (Patrón Comportamiento)
+		// Sirve para guardar y restaurar el estado interno de un objeto sin violar la encapsulación.
+		// GestorPartidas (Caretaker) -> gestor de checkpoints (Cuidador)
+		// Jugador (Originador)
+		// Memento (interfaz) // Contrato del memento
+		// MementoConcreto (Memento)
+		
+		
+
+	}
+
+}
---
+++ clase_04/src/ar/com/educacionit/memento/interfaces/Memento.java
@@ -0,0 +1,6 @@
+package ar.com.educacionit.memento.interfaces;
+
+public interface Memento {
+	public String getNivel();
+	public int getVida();
+}
